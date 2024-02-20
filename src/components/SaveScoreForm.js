6f1dcafc0d9278375e44b4328956753d270742c5
@jerscape <jeremydutton5@gmail.com>
2024-02-19 19:04:41 -0500

    partial commit

--- src/components/SaveScoreForm.js
+++ src/components/SaveScoreForm.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function SaveScoreForm(){
+export default function SaveScoreForm({ score }) {
+
+  const [username, setUsername] = useState('');
+
+    const onUserNameChange = (e) => {
+    const updatedUsername = e.target.value;
+    setUsername(updatedUsername);
+
+
+  };
+
+  const saveHighScore = (e) => {
+    e.preventDefault()
+    const record = {
+      name: username,
+      score
+    }
+    console.log(username)
+    console.log(record)
+  }
+
   return (
-    <form>
-      <input type="text" name="username" id="username" placeholder="cool kid 123"></input>
-      <button type="submit">Save</button>
-    </form>
-  )
+    <div>
+      <h1>Score:</h1>
+      <form  onSubmit={saveHighScore}>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="cool kid 123"
+          value={username}
+          onChange={onUserNameChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 }
